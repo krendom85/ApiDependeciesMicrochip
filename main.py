850b466086edef2133fd44f87ddee373f17f533f
Christian Moreno <95726720+krendom85@users.noreply.github.com>
2025-05-25 22:39:58 -0500

    cambio para manejo de lso dos zip de menos de 100 mb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-
-from fastapi import FastAPI, Depends, HTTPException,status
+import zipfile  # Cambiado de shutil a zipfile
+from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.responses import FileResponse
 from fastapi.security import HTTPBasicCredentials, HTTPBasic
 from fastapi.middleware.cors import CORSMiddleware
@@ -31,14 +31,25 @@ def authenticate_user(credentials: HTTPBasicCredentials = Depends(security)):
     return credentials
 
 @app.get("/download-dependecies/")
-async def Download(credentials: HTTPBasicCredentials = Depends(authenticate_user)):
-    zip_file_path = os.path.join("static", "ipe.zip")
+async def download(credentials: HTTPBasicCredentials = Depends(authenticate_user)):
+    # Archivos a incluir en el .zip
+    files = ["static/ipe.zip", "static/ipe.z01"]
+    zip_file_path = "static/ipe_combined.zip"
 
-    if not os.path.exists(zip_file_path):
-        return {"error": "File not found"}
-    
+    # Crear un archivo .zip temporal
+    try:
+        with zipfile.ZipFile(zip_file_path, "w") as zipf:  # Cambiado a zipfile.ZipFile
+            for file in files:
+                if os.path.exists(file):
+                    zipf.write(file, os.path.basename(file))
+                else:
+                    raise HTTPException(status_code=404, detail=f"Archivo {file} no encontrado")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error al crear el archivo .zip: {str(e)}")
+
+    # Enviar el archivo .zip como respuesta
     return FileResponse(
         path=zip_file_path,
         media_type="application/zip",
-        filename="ipe.zip"
+        filename="ipe_combined.zip"
     )
